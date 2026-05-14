1e7f03c67c888543be968b3b0fa7f7a8854f399b
Ivan <tibiegos@gmail.com>
2026-05-14 20:43:50 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	USDToEUR = 0.9
+	USDToRUB = 80.0
+)
+
+func main() {
+	EURToRUB := USDToRUB / USDToEUR
+
+	fmt.Printf("1 USD = %.2f EUR\n", USDToEUR)
+	fmt.Printf("1 USD = %.2f RUB\n", USDToRUB)
+	fmt.Printf("1 EUR = %.2f RUB\n", EURToRUB)
+}
